d84f7f281c4ae0867be8fc47530ad96d1051f419
stephane2018 <31846762+stephane2018@users.noreply.github.com>
2024-03-07 19:35:19 +0100

    Update check_port_and_stop_container.sh

--- check_port_and_stop_container.sh
+++ check_port_and_stop_container.sh
@@ -1,24 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Define the port to check
-port_to_check=3000
-
-# Check if the port is in use
-if netstat -tlpn | grep LISTEN | grep ":$port_to_check "; then
-  # Get the process ID (PID) using the port number
-  process_id=$(netstat -tlpn | grep LISTEN | grep ":$port_to_check " | awk '{print $NF}')
-
-  # Check if the process ID belongs to a container
-  if [[ $(docker inspect -f '{{.State.Pid}}' $process_id) == "$process_id" ]]; then
-    # Get the container ID using the process ID
-    container_id=$(docker ps -aqf="pid=$process_id")
-
-    # Stop the container
-    echo "Port $port_to_check is in use by container $container_id. Stopping the container..."
-    docker stop $container_id
-  else
-    echo "Port $port_to_check is in use by a non-container process (PID: $process_id)."
-  fi
-else
-  echo "Port $port_to_check is not currently in use."
-fi
+for id in $(docker ps -q)
+do
+    if [[ $(docker port "${id}") == *"${1}"* ]]; then
+        echo "stopping container ${id}"
+        docker stop "${id}"
+    fi
+done
