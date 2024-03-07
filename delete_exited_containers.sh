4dfaa2b552b94d93d3c5d1a9a4b18cab82adc726
stephane2018 <31846762+stephane2018@users.noreply.github.com>
2024-03-07 17:40:49 +0100

    Create delete_exited_containers.sh

---
+++ delete_exited_containers.sh
@@ -0,0 +1,17 @@
+function rem_image() {
+  image_id=$1
+  
+  docker rm -f $(docker ps -a -q --filter="ancestor=$image_id") 2>&- || echo "Found no containers for that image"
+  docker rmi $image_id
+  echo "Image deleted successfully"
+}
+
+function docker_clean() {
+  pattern=$1
+
+  for image_id in `docker images | grep $pattern | awk '{ print $3}'`
+  do
+      echo Removing... $image_id
+      rem_image $image_id
+  done
+}
